9a4ffd2f34e5c56abda74fd2e2113aca7e4af184
Gleb <glebkuimov@gmail.com>
2025-04-22 16:58:11 +0300

    dfs

--- main.py
+++ main.py
@@ -159,9 +159,9 @@ class Reservation:
 
         # Запускаем фоновую работу в фоне
         background_tasks = [
-            asyncio.create_task(self.check_arrivals(3600)),
-            asyncio.create_task(self.check_booking(3600)),
-            asyncio.create_task(self.check_available_flights(3600)),
+            # asyncio.create_task(self.check_arrivals(3600)),
+            # asyncio.create_task(self.check_booking(3600)),
+            # asyncio.create_task(self.check_available_flights(3600)),
             asyncio.create_task(self.fillup_sheets(3000))
         ]
 
